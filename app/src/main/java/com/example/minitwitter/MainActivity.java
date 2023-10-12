a65dc66d266f58c6374b6c0e65a8a231e8d5fcf3
Andrea Aguiire <4ndr3420@gmail.com>
2023-10-11 23:33:39 -0600

    Agregando pantalla de registro

--- app/src/main/java/com/example/minitwitter/MainActivity.java
+++ app/src/main/java/com/example/minitwitter/MainActivity.java
@@ -2,13 +2,48 @@ package com.example.minitwitter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnLogin;
+    TextView txtSignUp;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnLogin = findViewById(R.id.buttonLogin);
+        txtSignUp = findViewById(R.id.textViewGoSignUp);
+
+        btnLogin.setOnClickListener(this);
+        txtSignUp.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch (id) {
+            case R.id.buttonLogin:
+
+                break;
+
+            case R.id.textViewGoSignUp:
+                gotoSignup();
+                break;
+        }
+
+    }
+
+    private void gotoSignup() {
+        Intent i = new Intent(MainActivity.this, SignUpActivity.class);
+        startActivity(i);
+        finish();
     }
 }
---
+++ app/src/main/java/com/example/minitwitter/SignUpActivity.java
@@ -0,0 +1,50 @@
+package com.example.minitwitter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+public class SignUpActivity extends AppCompatActivity implements View.OnClickListener {
+
+    Button btnSingUp;
+    TextView txtLogin;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sing_up);
+
+        btnSingUp = findViewById(R.id.buttonSignUp);
+        txtLogin = findViewById(R.id.textViewLogin);
+
+        btnSingUp.setOnClickListener(this);
+        txtLogin.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        int id = v.getId();
+
+        switch(id){
+            case R.id.buttonSignUp:
+                break;
+            case R.id.textViewLogin:
+                gotoLogin();
+                break;
+        }
+    }
+
+    private void gotoLogin() {
+        Intent i = new Intent(SignUpActivity.this, MainActivity.class);
+        startActivity(i);
+        finish();
+    }
+}
